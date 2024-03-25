b253b3c56817be4a3a72a116744aedcadaf802d6
Gabriel Holmes <gabrielholmes2002@gmail.com>
2024-03-25 10:32:02 +0000

    adding content from lesson 4

---
+++ taskmanager/static/js/script.js
@@ -0,0 +1,5 @@
+document.addEventListener('DOMContentLoaded', function() {
+    // sidenac initialization
+    let sidenav = document.querySelectorAll('.sidenav');
+    M.Sidenav.init(sidenav);
+  });
